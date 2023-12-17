8aa18edc4cc0c44c9fb0f9086bddd9d0cbbf17e7
ImanAdithya <adithyaiman07@gmail.com>
2023-12-17 21:49:17 +0530

    content updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,21 +6,31 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-             backgroundColor: Colors.white,
-              title: Text('facebook',style: TextStyle(
-                color: Color(0xFF1777F2),
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: -1.2
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                  color: Color(0xFF1777F2),
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.2),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search),
+                iconSize: 30.0,
+                color: Colors.black,
               ),
-              ),
-              centerTitle: false,
-            )
-          ],
-        ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
